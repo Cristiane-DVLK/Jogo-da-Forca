67eb59202ba72093420e2a8ec37bc6c300659d47
Cristiane DVLK <71277550+Cristiane-DVLK@users.noreply.github.com>
2022-09-17 18:14:28 +0100

    Update forca.js

--- forca.js
+++ forca.js
@@ -6,203 +6,203 @@ let palavraSecretaSorteada;
 const palavras = [
 	palavra001 = {
 		nome: "IRLANDA",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra002 = {
 		nome: "EQUADOR",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra003 = {
 		nome: "CHILE",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra004 = {
 		nome: "INDONESIA",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra005 = {
 		nome: "MALDIVAS",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra006 = {
 		nome: "GROELANDIA",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra007 = {
 		nome: "UZBEQUISTAO",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra008 = {
 		nome: "AUSTRIA",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra009 = {
 		nome: "HAVAI",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra010 = {
 		nome: "KOPENHAGEN",
-		categoria: "LUGARES"
+		categoria: "Dica: LUGAR"
 	},
 	palavra011 = {
 		nnome: "BICICLETA",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra012 = {
 		nome: "LANCHA",
-		ccategoria: "TRANSPORTE"
+		ccategoria: "Dica: TRANSPORTE"
 	},
 	palavra013 = {
 		nome: "NAVIO",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra014 = {
 		nome: "TELEFERICO",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra015 = {
 		nome: "MOTOCICLETA",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra016 = {
 		nome: "BARCO",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra017 = {
 		nome: "AERONAVE",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra018 = {
 		nome: "TREM",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra019 = {
 		nome: "CAIAQUE",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra020 = {
 		nome: "ONIBUS",
-		categoria: "TRANSPORTE"
+		categoria: "Dica: TRANSPORTE"
 	},
 	palavra021 = {
 		nome: "SINO",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra022 = {
 		nome: "CHAVEIRO",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra023 = {
 		nome: "LAMPADA",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra024 = {
 		nome: "CORTINA",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra025 = {
 		nome: "LAPIS",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra026 = {
 		nome: "APONTADOR",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra027 = {
 		nome: "ESPADA",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra028 = {
 		nome: "MONITOR",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra029 = {
 		nome: "VARAL",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra030 = {
 		nome: "AGULHA",
-		categoria: "OBJETOS"
+		categoria: "Dica: OBJETO"
 	},
 	palavra031 = {
 		nome: "MELANCIA",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra032 = {
 		nome: "CASTANHA",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra033 = {
 		nome: "KIBE",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra034 = {
 		nome: "ALMONDEGA",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra035 = {
 		nome: "SAGU",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra036 = {
 		nome: "CEREJA",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra037 = {
 		nome: "POPOCA",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTOS"
 	},
 	palavra038 = {
 		nome: "FILE",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra039 = {
 		nome: "MANGA",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra040 = {
 		nome: "PRETZEL",
-		categoria: "ALIMENTOS"
+		categoria: "Dica: ALIMENTO"
 	},
 	palavra041 = {
 		nome: "MOTOCICLISMO",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra042 = {
 		nome: "BASEBALL",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra043 = {
 		nome: "ESGRIMA",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra044 = {
 		nome: "HIPISMO",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra045 = {
 		nome: "ATLETISMO",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra046 = {
 		nome: "ALTEROFILISMO",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra047 = {
 		nome: "GOLF",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra048 = {
 		nome: "RALLY",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra049 = {
 		nome: "BALE",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	},
 	palavra050 = {
 		nome: "REMO",
-		categoria: "ESPORTES"
+		categoria: "Dica: ESPORTE"
 	}
 ];
 
@@ -260,7 +260,7 @@ function verificaLetraEscolhida(letra){
 function mudarStyleLetra(tecla, condicao){
     if(condicao == false)
     {
-        document.getElementById(tecla).style.background = "#C71585";
+        document.getElementById(tecla).style.background = "grey";
         document.getElementById(tecla).style.color = "#ffffff";
     }
     else{
@@ -307,25 +307,25 @@ function comparalistas(letra){
 function carregaImagemForca(){
     switch(tentativas){
         case 5:
-            document.getElementById("imagem").style.background  = "url('../forca/forca11.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto2.png')";
             break;
         case 4:
-            document.getElementById("imagem").style.background  = "url('../forca/forca22.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto3.png')";
             break;
         case 3:
-            document.getElementById("imagem").style.background  = "url('../forca/forca33.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto4.png')";
             break;
         case 2:
-            document.getElementById("imagem").style.background  = "url('../forca/forca44.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto5.png')";
             break;
         case 1:
-            document.getElementById("imagem").style.background  = "url('../forca/forca55.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto6.png')";
             break;
         case 0:
-            document.getElementById("imagem").style.background  = "url('../forca/forca66.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto6.png')";
             break;
         default:
-            document.getElementById("imagem").style.background  = "url('./img/forca.png')";
+            document.getElementById("imagem").style.background  = "url('../homem-e-jacare/meu-projeto1.png')";
             break;
     }
 }
@@ -346,6 +346,3 @@ let bntReiniciar = document.querySelector("#btnReiniciar")
 bntReiniciar.addEventListener("click", function(){
     location.reload();
 });
-
-
-
